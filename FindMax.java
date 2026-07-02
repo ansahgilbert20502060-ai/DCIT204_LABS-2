3bf0d415b576fc804ccf775ae4903eda7d95fac9
ansahgilbert20502060-ai <ansahgilbert20502060@gmail.com>
2026-07-02 01:43:07 +0000

    Create FindMax.java

---
+++ FindMax.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class FindMax {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = input.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
+
+        int max = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("The maximum value is: " + max);
+        System.out.println("Time Complexity: O(n)");
+
+        input.close();
+    }
+}
